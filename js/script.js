16e2509e8ad1b3f0db3248eebe0844c2d6e47bcd
Adrian Derlatka <adrian.derlatka@rhenus.com>
2024-11-23 18:34:58 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let returnOrder = document.getElementById("rmaNumber");
+let returnArticle = document.querySelector(".js-article");
+let form1 = document.querySelector(".js-form-1");
+let articleElement = document.querySelector(".js-Article");
+let orderElement = document.querySelector(".js-Order");
+
+addEventListener("input", ()=> {
+    orderElement.innerText = returnOrder.value;
+})
